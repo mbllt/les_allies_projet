2e79b5e2101e9f07957a28f0ca6c0386b66a4ccd
mbllt <ballet.mia.6@gmail.com>
2022-12-30 16:34:47 +0100

    bar nav sur la page reveal et reseaux sociaux pas de lien

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -4,14 +4,12 @@ import './Info.css';
 const Info = () => {
 	return (
 		<div className='info'>
-			<div className='vec4box'>
-			</div>
 			<img className='slogan_info' src="slogan.png" alt="slogan"/>
 			<div className='text_info'>
 				<p className='p1'>L’image et la réputation de votre entreprise conditionne<br />sa performance et sa croissance.</p>
 				<p className='p2'>C’est pourquoi l’agence <strong className='color_text' style={{color:'#E3C0FF'}}>Les Alliés</strong> crée des stratégies de<br />communication globale et sur-mesure.</p>
 			</div>
-			<div className='vec5box'>
+			<div className='vec5-6box'>
 				<img className='vector_info' src="vector5-6.svg" alt="vector"/>
 			</div>
 		</div>
--- src/pages/NewSite.tsx
+++ src/pages/NewSite.tsx
@@ -4,18 +4,16 @@ import './NewSite.css';
 const NewSite = () => {
 	return (
 		<div className='newsite'>
-			<div className="text_vector">
-				<article className="text">
-					<p>L’agence se réinvente !</p>
-					<p>En attendant de découvrir notre nouveau site internet,</p>
-					<p>découvrez notre nouvelle identité</p>
-				</article>
+			<div className="text">
+				<p>L’agence se réinvente !</p>
+				<p>En attendant de découvrir notre nouveau site internet,</p>
+				<p>découvrez notre nouvelle identité</p>
 			</div>
 			<div className="logo">
 				<img className='logo_newsite image-main' src="logo.png" alt="logo"/>
 				<img className='logo_newsite image-hover' src="logo_purple.png" alt="logo_purple"/>
 			</div>
-			<div className='vec3box'>
+			<div className='vec3-4box'>
 				<img className='vector_newsite' src="vector3-4.svg" alt="vector"/>
 			</div>
 		</div>
