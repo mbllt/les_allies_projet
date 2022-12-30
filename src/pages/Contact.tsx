da3c2bcaeac4555c98acceeb747029fbfb936c9f
mbllt <ballet.mia.6@gmail.com>
2022-12-30 19:12:44 +0100

    text size bigger and contact color hover

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -9,13 +9,13 @@ const Contact = () => {
 				<img className='nous' src="nous.png" alt="nous"/>
 			</div>
 			<div className='text_box'>
-				<p className='text_contact'>Basée en région lyonnaise,<br />nous serions ravies de vous <strong className='color_text' style={{color:'#E3C0FF'}}>accompagnez</strong> ou<br/>simplement de <strong className='color_text' style={{color:'#E3C0FF'}}>répondre à vos questions.</strong></p>
+				<p className='text_contact'>Basée en région lyonnaise, nous serions ravies de vous <strong className='color_text' style={{color:'#E3C0FF'}}>accompagnez</strong> ou simplement de <strong className='color_text' style={{color:'#E3C0FF'}}>répondre à vos questions.</strong></p>
 			</div>
 			<div className='link_contact'>
 				<a className='mail_contact'>noemie@lesalliésdelacom.com</a>
 				<a className='phone_contact'>06.32.95.14.76</a>
-				<a>instagram</a>
-				<a>indeed</a>
+				<a className='insta'>instagram</a>
+				<a className='indeed'>indeed</a>
 			</div>
 		</div>
 	);
