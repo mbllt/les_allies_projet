81ceb8712dbb978e6dbd62810f02fef45a43d1b4
mbllt <ballet.mia.6@gmail.com>
2023-01-03 23:23:34 +0100

    first push

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -8,17 +8,35 @@ import Contact from '../pages/Contact';
 function App() {
   return (
     <div className="App">
+
+      {/* TOP */}
       <div className='top'>
 				<a href='#newsite'>Reveal</a>
 				<img className='small_logo_newsite' src="small_logo.png" alt="small_logo_la"/>
 				<a href='#contact'>Contact</a>
 			</div>
+      {/* END */}
+
+      {/* NEWSITE */}
+      {/* <div style={{textAlign:'center'}}>
+        <p className='text_reveal'>Bâtisseur de marque,<br /> Accélérateur de <span style={{fontFamily:'gotcha-regular', fontSize:150, color:"#E3C0FF"}}>business</span></p>
+      </div>
+      <img className='vector_reveal' src="vector1-2.svg" alt="vector"/> */}
       <Reveal/>
-      <NewSite/>
+      {/* END */}
+
+      {/* REVEAL */}
+      <p className='paraph1'>L’agence se réinvente ! <br/>
+				En attendant de découvrir notre nouveau site internet, <br/>
+				découvrez notre nouvelle identité</p>
+      {/* END */}
+
+      {/* <NewSite/>
       <Info/>
-      <Contact/>
+      <Contact/> */}
     </div>
   )
 }
 
 export default App
+
--- src/pages/Reveal.tsx
+++ src/pages/Reveal.tsx
@@ -4,11 +4,10 @@ import './Reveal.css';
 const Reveal = () => {
 	return (
 		<div id='reveal'>
-			<div className="central_text">
-				<p className='text_reveal'><strong>Bâtisseur de marque,<br />Accélérateur de</strong></p>
-				<img className='business_reveal' src="business.png" alt="business_text"/>
-				<img className='vector_reveal' src="vector1-2.svg" alt="vector"/>
+			<div style={{textAlign:'center'}}>
+				<p className='text_reveal'>Bâtisseur de marque,<br /> Accélérateur de <span style={{fontFamily:'gotcha-regular', fontSize:150, color:"#E3C0FF"}}>business</span></p>
 			</div>
+			<img className='vector_reveal' src="vector1-2.svg" alt="vector"/>
 		</div>
 	);
 };
