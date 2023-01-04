5cb435e957d278b8348a36f0f2166068b3ba850f
mbllt <ballet.mia.6@gmail.com>
2023-01-04 15:52:38 +0100

    je bosse sur le responsive de la nouvelle page 1

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -8,31 +8,8 @@ import Contact from '../pages/Contact';
 
 function App() {
 
-    window.addEventListener("scroll", function() {
-      const navbar = document.querySelector("top");
-      navbar ? navbar.classList.toggle("sticky", window.scrollY > 0) : navbar;
-
-    })
-
-  // window.onscroll = function() {scrollFunction()};
-  
-  // function scrollFunction() {
-  //   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-  //     const navbar = document.querySelector("top");
-  //     navbar ? navbar.classList.toggle("sticky", window.scrollY > 0) : navbar;
-  //   } else {
-  //     const navbar = document.getElementById("top");
-  //     navbar ? navbar.style.color = "blue" : navbar;
-  //   }
-  // }
-
   return (
     <div className="App">
-      {/* <div className='top'>
-				<a href='#newsite'>Reveal</a>
-				<img className='small_logo_newsite' src="/assets/logo/small_logo.png" alt="small_logo_la"/>
-				<a href='#contact'>Contact</a>
-			</div> */}
       <NavBar/>
       <Reveal/>
       {/* <NewSite/>
--- src/pages/Reveal.tsx
+++ src/pages/Reveal.tsx
@@ -4,9 +4,13 @@ import './Reveal.css';
 const Reveal = () => {
 	return (
 		<div id='reveal'>
-			<div className='box_text_reveal' style={{textAlign:'center'}}>
-				<p className='text_reveal'>Branding,<br />Formation,<br/><span className='business'>Communication</span></p>
+			<div className='box reveal'>
+				<p className='slogan reveal'>Branding,<br />Formation,<br/><span id='communication'>Communication</span></p>
 				<p className='agile'>agile</p>
+				<p className='text reveal'>L’image et la réputation de votre entreprise<br/>
+					conditionnent sa performance et sa croissance.<br/>
+					C’est pourquoi l’agence <span id="allies">Les Alliés</span> crée des stratégies<br/>
+					de communication globales et sur-mesures.</p>
 			</div>
 			<img className='vector_reveal' src="/assets/vec/vec1.svg" alt="vector"/>
 		</div>
