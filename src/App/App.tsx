c65e5145f156b43df9064da9013ab1d4568d2eb5
mbllt <ballet.mia.6@gmail.com>
2023-01-16 14:32:24 +0100

    plus qua monte la premiere page et on est bon

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -16,8 +16,8 @@ function App() {
       <Reveal/>
   		<img className='vector2' src="/assets/vec/vec2.svg" alt="vector"/>
       <Info/>
-          <source srcSet="/assets/vec/vec3.svg" media="(max-width: 500px)"/>
-        <img className='vector3' src="/assets/vec/vec3.svg" alt="vector"/>
+          <source srcSet="/assets/vec/newvec3.svg" media="(max-width: 500px)"/>
+        <img className='vector3' src="/assets/vec/newvec3.svg" alt="vector"/>
       <Contact/>
     </div>
   )
