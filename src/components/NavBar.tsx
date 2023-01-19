9ee6de9aa9c0370cdbec3a23af6fb102315c99d1
mbllt <ballet.mia.6@gmail.com>
2023-01-19 17:01:44 +0100

    premiere modif

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,10 +5,9 @@ function NavBar() {
 	return (
 		<div className='navbar'>
 			<div className='content_navbar'>
-				<a href='#reveal'>reveal</a>
-				{/* <a href='#vector1'>reveal</a> */}
+				<a href='#reveal'>Reveal</a>
 				<img className='small_logo_newsite' src="/assets/logo/small_logo.png" alt="small_logo_la"/>
-				<a href='#contact'>contact</a>
+				<a href='#contact'>Contact</a>
 			</div>
 		</div>
 	);
